7688937bb9247141842afb0bbfcfcbd8eec662d7
Joris Pevcevičius <joris.pevcas@gmail.com>
2026-02-27 20:00:26 +0200

    Add Location, group computers by location in index page

--- ctrl/models.py
+++ ctrl/models.py
@@ -2,9 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Location(models.Model):
+    name = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Computer(models.Model):
     machine_id = models.CharField(max_length=40, db_index=True)
     name = models.CharField(max_length=32)
+    location = models.ForeignKey(Location, null=True, blank=True, on_delete=models.SET_NULL)
 
     @property
     def most_recent_checkin(self):
