9ca3ae0d05be5354e11bc6459b67e0da56b62386
Joris Pevcevičius <joris.pevcas@gmail.com>
2026-03-10 16:07:51 +0200

    Fix date and datetime formats

--- ctrl_srv/settings.py
+++ ctrl_srv/settings.py
@@ -108,10 +108,7 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Europe/Vilnius'
 
-USE_I18N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
@@ -124,3 +121,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/admin/login/'
+
+USE_I18N = False
+USE_L10N = False
+DATE_FORMAT = "Y-m-d"
+DATETIME_FORMAT = "Y-m-d H:i:s"
+SHORT_DATE_FORMAT = "Y-m-d"
+SHORT_DATETIME_FORMAT = "Y-m-d H:i:s"
