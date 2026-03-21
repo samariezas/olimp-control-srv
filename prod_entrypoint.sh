36bec4b5973496e7df34e5375af040aadece5488
Joris Pevcevičius <joris.pevcas@gmail.com>
2026-03-21 12:19:32 +0200

    Fix gunicorn control socket permission denied

--- prod_entrypoint.sh
+++ prod_entrypoint.sh
@@ -7,4 +7,4 @@ python manage.py collectstatic --clear --noinput
 python manage.py migrate --noinput
 
 # for worker count, see: https://gunicorn.org/design/#how-many-workers
-su ctrl -c "python -m gunicorn --bind 0.0.0.0:8000 --workers $(( 2 * $(nproc) + 1 )) ctrl_srv.wsgi:application"
+su ctrl -c "python -m gunicorn --control-socket /tmp/gunicorn_ctrl.ctl --bind 0.0.0.0:8000 --workers $(( 2 * $(nproc) + 1 )) ctrl_srv.wsgi:application"
